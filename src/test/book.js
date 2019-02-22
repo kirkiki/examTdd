f68a3f46792174586c33db659489b2ffd1bf5de1
zoubi <moricio123@protonmail.com>
2019-02-22 17:06:27 +0100

    final version

--- src/test/book.js
+++ src/test/book.js
@@ -102,6 +102,70 @@ describe('Test intégration2 (Empty database)', () => {
     });
 });
 
+describe('Test intégration2 (Empty database)', () => {
+
+    it('nock tests get', function(done) {
+        nock('http://localhost:8080')
+        .get('/book/0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9')
+        .reply(200, {"books": []})
+        
+        chai.request('http://localhost:8080')
+        .get('/book/0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9')
+        .end((err, res) => {
+            if (err) console.log(err)
+                expect(res).to.have.status(200)
+                expect(res.body.books).to.be.a('array')
+                done()
+        })
+    });
+    it('nock tests post', function(done) {
+        nock('http://localhost:8080')
+        .post('/book')
+        .reply(200, {"message": "book successfully added"})
+        
+        chai.request('http://localhost:8080')
+        .post('/book')
+        .send({
+                'title': 'Coco raconte Channel 2',
+                'years': 1990,
+                'pages': 400
+            })
+        .end(function(err, res) {
+            expect(res).to.have.status(200);
+            expect(res.body.message).to.contains('book successfully added')
+            done();                               
+        });
+    });
+    it('nock tests put', function(done) {
+        nock('http://localhost:8080')
+        .put('/book/0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9')
+        .reply(200, {"message": "book successfully updated"})
+        
+        chai.request('http://localhost:8080')
+        .put('/book/0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9')
+        .end((err, res) => {
+            if (err) console.log(err)
+                expect(res).to.have.status(200)
+                expect(res.body.message).to.contains('book successfully updated')
+                done()
+        })
+    });
+    it('nock tests put', function(done) {
+        nock('http://localhost:8080')
+        .delete('/book/0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9')
+        .reply(200, {"message": "book successfully deleted"})
+        
+        chai.request('http://localhost:8080')
+        .delete('/book/0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9')
+        .end((err, res) => {
+            if (err) console.log(err)
+                expect(res).to.have.status(200)
+                expect(res.body.message).to.contains('book successfully deleted')
+                done()
+        })
+    });
+});
+
 
 
 // tout les packages et fonction nescessaire au test sont importé ici, bon courage
